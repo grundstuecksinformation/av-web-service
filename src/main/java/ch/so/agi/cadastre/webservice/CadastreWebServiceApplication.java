baf01e43ce0bf91064905fed53f802ea039df008
Stefan Ziegler <edi.gonzales@gmail.com>
2020-03-27 16:14:10 +0100

    initial commit

---
+++ src/main/java/ch/so/agi/cadastre/webservice/CadastreWebServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.cadastre.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastreWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastreWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/so/agi/cadastre/webservice/CadastreWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi.cadastre.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastreWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
